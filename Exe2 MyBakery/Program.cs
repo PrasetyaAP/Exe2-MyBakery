f14e0d19fdc7c3f30a7cc4e61c1a42b70aa2e555
Prasetya Agung <prasetya.agung.ft20@mail.umy.ac.id>
2022-03-26 15:07:29 +0700

    Add project files.

---
+++ Exe2 MyBakery/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe2_MyBakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connection();
+        }
+
+        private void Connection()
+        {
+            using(
+                SqlConnection con = new SqlConnection("data source=LAPTOP-C5A9RCLU\\PRASETYAAGUNGP;database=MyBakery;user Id = sa;Password=123"))
+            {
+                con.Open();
+                Console.WriteLine("MASOK BOS Q!!!!");
+                Console.Read();
+            }
+        }
+    }
+}
